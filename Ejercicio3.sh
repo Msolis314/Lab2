04e158a389861bf5a379e17bd2183781c0284e33
mariana@marisoli <solisfanny2021@gmail.com>
2023-09-13 14:15:08 -0600

    Esta version funciona, pero no muestra el titulo y todavia esta el problema de que el comando acabe antes que el while

--- Ejercicio3.sh
+++ Ejercicio3.sh
@@ -2,7 +2,7 @@
 n=0
 $1 $2 &
 echo " #%CPU #MEM #TIME" > log.txt
-while [ $n -lt 30 ];
+while [ $n -lt 50 ];
 do
 	id=$( pidof -s  $1 $2 )
 	var1=$( ps -p $id -o %cpu= )
@@ -11,4 +11,11 @@ do
 	echo "$var1 $var2 $time" >> log.txt
 	n=$(( n+1))
 done
+
+#set title "%CPU"
+
+gnuplot --persist -e 'plot "log.txt" u 3:1'
+
+gnuplot --persist -e 'plot "log.txt" u 2:1'
+
 
