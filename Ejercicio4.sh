fd410c618237f7ffb7b5772b315fa75f483ce4b3
mariana@marisoli <solisfanny2021@gmail.com>
2023-09-13 15:37:32 -0600

    Version definitiva :)

--- Ejercicio4.sh
+++ Ejercicio4.sh
@@ -6,7 +6,7 @@ echo "Monitoreo de Prueba" >  log2.txt
 while true;
 do
 	
-	if  inotifywait /home/mariana/Lab2/Prueba   ; then
+	if  inotifywait -q -q /home/mariana/Lab2/Prueba 1> /dev/null  ; then
 		echo " Fecha : $( date "+%D" ) Hora: $( date "+%T" )" >> log2.txt
 	fi
 
